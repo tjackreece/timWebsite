25c0aecbcce160f1056375179749fdae01687cbf
Wynstona Louis <wynstona@Wynstonas-MacBook-Pro.local>
2021-10-13 16:50:07 -0400

    done

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./routesCss/About.css";
-import file from "../components/componentsData/imgFiles/resume1.jpg";
+import file from "../components/componentsData/imgFiles/Timothy_Jackreece_Resume.pdf";
 
 const About = () => {
 	return (
