f910f4b31f4d703d7df61e7d2f15f182850fc49a
Wynstona Louis <wynstona@Wynstonas-MacBook-Pro.local>
2021-10-13 16:45:01 -0400

    done

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./routesCss/About.css";
-import file from "../components/componentsData/imgFiles/resume1.jeg";
+import file from "../components/componentsData/imgFiles/resume1.jpg";
 
 const About = () => {
 	return (
